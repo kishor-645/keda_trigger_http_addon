63d98197172826eed446a5f364bc117d7273a274
kishor vaishanav <75803767+kishor-645@users.noreply.github.com>
2026-05-05 17:38:58 +0530

    Update installkeda.sh

--- installkeda.sh
+++ installkeda.sh
@@ -3,4 +3,4 @@
 helm repo add kedacore https://kedacore.github.io/charts
 helm repo update
 helm install keda kedacore/keda --namespace keda --create-namespace
-helm install http-add-on kedacore/keda-add-ons-http --namespace keda
+helm install http-add-on kedacore/keda-add-ons-http  -f values.yaml --namespace keda
